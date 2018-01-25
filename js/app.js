ff9bf735d314b4a6918b6cd948d72e2d08f35e65
Gaurav Rai <grchamlin@gmail.com>
2018-01-25 15:01:37 -0600

    inital commit with sidebar

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// $('.ui .sidebar')
+//   .sidebar()
+//   .sidebar({
+//
+//     dimPage: false,
+//     closable: false
+//   })
+//   .sidebar('attach events','.ui.launch.button');
+
+  $(".ui.sidebar")
+    .sidebar({
+      dimPage: false,
+      closable: false,
+      
+    })
+    .sidebar('attach events','.ui.launch.button');
+//
+//
+//   $('.ui.sidebar')
+//     .sidebar('setting', 'transition', 'push')
+//     .sidebar('toggle')
+//   ;
